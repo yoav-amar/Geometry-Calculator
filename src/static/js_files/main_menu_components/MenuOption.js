845576c40b61a99b6938622f7a57d0d4d7163ed7
nadavsrb <69578111+nadavsrb@users.noreply.github.com>
2021-12-18 17:46:38 +0200

    making the structure for the menu

---
+++ src/static/js_files/main_menu_components/MenuOption.js
@@ -0,0 +1,15 @@
+class MenuOption extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+        <div className="menu_option">
+            <button>
+              Hello!!
+            </button>
+        </div>
+    );
+  }
+}
