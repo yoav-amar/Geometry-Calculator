773696f8e7cd239998501b9e947e2310029d0d45
nadavsrb <69578111+nadavsrb@users.noreply.github.com>
2022-06-24 22:32:57 +0300

    more beutefull

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import MainMenu from 'js_files/main_menu/main_menu'
+import "./css_files/index.css";
 
 let domContainers = document.querySelectorAll('.main_menu')
 domContainers.forEach(domContainer => ReactDOM.render(<MainMenu/>, domContainer))
